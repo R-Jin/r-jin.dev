b9e0ed0353ff05f56f188fc7e8370990f10b0f21
R-Jin <ryanjin007@gmail.com>
2024-01-22 21:57:02 +0100

    Change footer text color

--- src/ui/components/footer/Footer.tsx
+++ src/ui/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import Socials from "../Socials";
 
 const Footer = () => {
   return (
-    <footer className="dark:text-darkforeground-50 flex flex-col items-center space-y-4 p-4 text-xs">
+    <footer className="flex flex-col items-center space-y-4 p-4 text-xs text-foreground">
       <Socials />
       <p className="center">Made by Ryan Jin, Powered by Vercel</p>
     </footer>
