1dec5145cacd364c29cd60f97f6d6087617b8ac7
r-jin <ryanjin007@gmail.com>
2026-02-18 23:38:33 +0100

    script for pushing

---
+++ utils/push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BLOG_REPO="/home/r-jin/code/r-jin"
+
+cd "$BLOG_REPO" || exit
+
+# Check if there are actually changes to avoid empty commits
+if [[ -z $(git status --porcelain) ]]; then
+    echo "No changes detected. Skipping push."
+    exit 0
+fi
+
+# The Git Workflow
+echo "Changes detected. Pushing to remote..."
+git add .
+git commit -m "Content update: $(date +'%Y-%m-%d %H:%M:%S')"
+git push origin main
+
+echo "Deployment complete!"
