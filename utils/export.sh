045b3ff56034148fdc255a0f45c3d9364c222074
r-jin <ryanjin007@gmail.com>
2026-02-18 14:10:44 +0100

    Create script used to export Obsidian md to md that is acceptable by Hugo

--- utils/export.sh
+++ utils/export.sh
@@ -3,5 +3,4 @@
 rm -rf /home/r-jin/code/r-jin/content/*
 
 # Run the export
-# --hard-linebreaks is often good for Obsidian users
 obsidian-export "/mnt/c/Users/Ryan/Documents/Second Brain/6. Blog" /home/r-jin/code/r-jin/content
